82b2712eaca21da96cc19c688469014b55ac97cb
Antoine de Prieelle <antoine_@live.nl>
2023-02-25 16:05:56 +0100

    Add react component Header and add scss styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './App.module.scss';
+import { Header } from '../Header/Header.js';
 
 export const App = () => {
   return (
     <div className={styles.App}>
-        test
+        <Header title={"Binary Search Tree"} />
     </div>
   );
 };
