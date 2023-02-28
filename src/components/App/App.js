17fecb9a34aa83a8bf68447d54c13eb108aa70c0
Antoine de Prieelle <antoine_@live.nl>
2023-02-28 15:03:53 +0100

    Add component ControlsBST with scss styling and add font to index.html

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './App.module.scss';
 import { Header } from '../Header/Header.js';
+import { ControlsBST } from '../ControlsBST/ControlsBST.js';
 
 export const App = () => {
   return (
     <div className={styles.App}>
         <Header title={"Binary Search Tree"} />
+        <ControlsBST />
     </div>
   );
 };
---
+++ src/components/ControlsBST/ControlsBST.js
@@ -0,0 +1,28 @@
+import styles from './ControlsBST.module.scss';
+
+export const ControlsBST = () => {
+  return (
+    <div className={styles.ControlsBST}>
+      <form className={styles.ControlsBST__form}>
+          <label className={styles["ControlsBST__form-label"]} htmlFor="initial-values">Add initial value(s) followed by a comma and a space</label>
+          <input className={styles["ControlsBST__form-input"]} id="initial-values" name="initial-values" type="text" />
+          <button className={styles["ControlsBST__form-button"]}>Create BST</button>
+          <label className={styles["ControlsBST__form-label"]} htmlFor="insert-values">Add consecutive value(s) followed by a comma and a space</label>
+          <input className={styles["ControlsBST__form-input"]} id="insert-values" name="insert-values" type="text" />
+          <button className={styles["ControlsBST__form-button"]}>Add values</button>
+          <label className={styles["ControlsBST__form-label"]} htmlFor="remove-values">Remove value(s) followed by a comma and a space</label>
+          <input className={styles["ControlsBST__form-input"]} id="remove-values" name="remove-values" type="text" />
+          <button className={styles["ControlsBST__form-button"]}>Remove values</button>
+          <label className={styles["ControlsBST__form-label"]} htmlFor="depth-value">Find the depth of a value in the BST</label>
+          <input className={styles["ControlsBST__form-input"]} id="depth-value" name="depth-value" type="text" />
+          <button className={styles["ControlsBST__form-button"]}>Find depth</button>
+          <div className={styles["ControlsBST__form-result"]}>The depth of the given value in the BST is: {"result-placeholder"}</div>
+          <label className={styles["ControlsBST__form-label"]} htmlFor="height-value">Find the height of a value in the BST</label>
+          <input className={styles["ControlsBST__form-input"]} id="height-value" name="depth-value" type="text" />
+          <button className={styles["ControlsBST__form-button"]}>Find height</button>
+          <div className={styles["ControlsBST__form-result"]}>The height of the given value in the BST is: {"result-placeholder"}</div>
+          <button className={styles["ControlsBST__form-button"]}>Rebalance BST</button>
+      </form>
+    </div>
+  );
+};
