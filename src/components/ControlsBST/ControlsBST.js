6cf8d3cbef4323792047af95b4c59bd641f9a56b
Antoine de Prieelle <antoine_@live.nl>
2023-03-07 15:04:47 +0100

    Add scss styling for positioning of fontawsome icons

--- src/components/ControlsBST/ControlsBST.js
+++ src/components/ControlsBST/ControlsBST.js
@@ -7,28 +7,46 @@ export const ControlsBST = () => {
     <div className={styles.ControlsBST}>
       <form className={styles.ControlsBST__form}>
           <label className={`${styles["ControlsBST__form-label"]} ${styles["ControlsBST__form-label-1"]}`} htmlFor="initial-values">Add initial value(s) followed by a comma and a space</label>
-          <input className={`${styles["ControlsBST__form-input"]} ${styles["ControlsBST__form-input-1"]} ${styles["ControlsBST__form-input--error"]}`} id="initial-values" name="initial-values" type="text" />
+          <div className={`${styles["ControlsBST__form-input-icon-wrapper"]} ${styles["ControlsBST__form-input-icon-wrapper-1"]}`}>
+            <input className={`${styles["ControlsBST__form-input"]} ${styles["ControlsBST__form-input--error"]}`} id="initial-values" name="initial-values" type="text" />
+            {/* <FontAwesomeIcon icon={faCircleCheck} className={`${styles["ControlsBST__form-input-icon"]} ${styles["ControlsBST__form-input-icon--success"]}`}/> */}
+            <FontAwesomeIcon icon={faCircleExclamation} className={`${styles["ControlsBST__form-input-icon"]} ${styles["ControlsBST__form-input-icon--error"]}`} />
+          </div>
           <button className={`${styles["ControlsBST__form-button"]} ${styles["ControlsBST__form-button-1"]}`}>Create BST</button>
           <div className={`${styles["ControlsBST__form-message"]} ${styles["ControlsBST__form-message-1"]} ${styles["ControlsBST__form-message--error"]}`}>Please enter the initial values in the correct format</div>
           <label className={`${styles["ControlsBST__form-label"]} ${styles["ControlsBST__form-label-2"]}`} htmlFor="insert-values">Add consecutive value(s) followed by a comma and a space</label>
-          <input className={`${styles["ControlsBST__form-input"]} ${styles["ControlsBST__form-input-2"]} ${styles["ControlsBST__form-input--success"]}`} id="insert-values" name="insert-values" type="text" />
+          <div className={`${styles["ControlsBST__form-input-icon-wrapper"]} ${styles["ControlsBST__form-input-icon-wrapper-2"]}`}>
+            <input className={`${styles["ControlsBST__form-input"]} ${styles["ControlsBST__form-input--success"]}`} id="insert-values" name="insert-values" type="text" />
+            <FontAwesomeIcon icon={faCircleCheck} className={`${styles["ControlsBST__form-input-icon"]} ${styles["ControlsBST__form-input-icon--success"]}`}/>
+            {/* <FontAwesomeIcon icon={faCircleExclamation} className={`${styles["ControlsBST__form-input-icon"]} ${styles["ControlsBST__form-input-icon--error"]}`} /> */}
+          </div>
           <button className={`${styles["ControlsBST__form-button"]} ${styles["ControlsBST__form-button-2"]}`}>Add values</button>
           <div className={`${styles["ControlsBST__form-message"]} ${styles["ControlsBST__form-message-2"]} ${styles["ControlsBST__form-message--success"]}`}>Please enter the consecutive values in the correct format</div>
           <label className={`${styles["ControlsBST__form-label"]} ${styles["ControlsBST__form-label-3"]}`} htmlFor="remove-values">Remove value(s) followed by a comma and a space</label>
-          <input className={`${styles["ControlsBST__form-input"]} ${styles["ControlsBST__form-input-3"]}`} id="remove-values" name="remove-values" type="text" />
+          <div className={`${styles["ControlsBST__form-input-icon-wrapper"]} ${styles["ControlsBST__form-input-icon-wrapper-3"]}`}>
+            <input className={`${styles["ControlsBST__form-input"]}`} id="remove-values" name="remove-values" type="text" />
+            {/* <FontAwesomeIcon icon={faCircleCheck} className={`${styles["ControlsBST__form-input-icon"]} ${styles["ControlsBST__form-input-icon--success"]}`}/> */}
+            <FontAwesomeIcon icon={faCircleExclamation} className={`${styles["ControlsBST__form-input-icon"]} ${styles["ControlsBST__form-input-icon--error"]}`} />
+          </div>
           <button className={`${styles["ControlsBST__form-button"]} ${styles["ControlsBST__form-button-3"]}`}>Remove values</button>
           <div className={`${styles["ControlsBST__form-message"]} ${styles["ControlsBST__form-message-3"]}`}>Please enter the remove values in the correct format</div>
           <label className={`${styles["ControlsBST__form-label"]} ${styles["ControlsBST__form-label-4"]}`} htmlFor="depth-value">Find the depth of a value in the BST</label>
-          <input className={`${styles["ControlsBST__form-input"]} ${styles["ControlsBST__form-input-4"]}`} id="depth-value" name="depth-value" type="text" />
+          <div className={`${styles["ControlsBST__form-input-icon-wrapper"]} ${styles["ControlsBST__form-input-icon-wrapper-4"]}`}>
+            <input className={`${styles["ControlsBST__form-input"]}`} id="depth-value" name="depth-value" type="text" />
+            {/* <FontAwesomeIcon icon={faCircleCheck} className={`${styles["ControlsBST__form-input-icon"]} ${styles["ControlsBST__form-input-icon--success"]}`}/> */}
+            <FontAwesomeIcon icon={faCircleExclamation} className={`${styles["ControlsBST__form-input-icon"]} ${styles["ControlsBST__form-input-icon--error"]}`} />
+          </div>
           <button className={`${styles["ControlsBST__form-button"]} ${styles["ControlsBST__form-button-4"]}`}>Find depth</button>
           <div className={`${styles["ControlsBST__form-message"]} ${styles["ControlsBST__form-message-4"]}`}>The depth of the given value in the BST is: {"result-placeholder"}</div>
           <label className={`${styles["ControlsBST__form-label"]} ${styles["ControlsBST__form-label-5"]}`} htmlFor="height-value">Find the height of a value in the BST</label>
-          <input className={`${styles["ControlsBST__form-input"]} ${styles["ControlsBST__form-input-5"]}`} id="height-value" name="depth-value" type="text" />
+          <div className={`${styles["ControlsBST__form-input-icon-wrapper"]} ${styles["ControlsBST__form-input-icon-wrapper-5"]}`}>
+            <input className={`${styles["ControlsBST__form-input"]}`} id="height-value" name="depth-value" type="text" />
+            {/* <FontAwesomeIcon icon={faCircleCheck} className={`${styles["ControlsBST__form-input-icon"]} ${styles["ControlsBST__form-input-icon--success"]}`}/> */}
+            <FontAwesomeIcon icon={faCircleExclamation} className={`${styles["ControlsBST__form-input-icon"]} ${styles["ControlsBST__form-input-icon--error"]}`} />
+          </div>
           <button className={`${styles["ControlsBST__form-button"]} ${styles["ControlsBST__form-button-5"]}`}>Find height</button>
           <div className={`${styles["ControlsBST__form-message"]} ${styles["ControlsBST__form-message-5"]}`}>The height of the given value in the BST is: {"result-placeholder"}</div>
           <button className={`${styles["ControlsBST__form-button"]} ${styles["ControlsBST__form-button--large"]}`}>Rebalance BST</button>
-          <FontAwesomeIcon icon={faCircleCheck} className={`${styles["ControlsBST__form-input-icon"]} ${styles["ControlsBST__form-input-icon--success"]}`}/>
-          <FontAwesomeIcon icon={faCircleExclamation} className={`${styles["ControlsBST__form-input-icon"]} ${styles["ControlsBST__form-input-icon--error"]}`} />
       </form>
     </div>
   );
