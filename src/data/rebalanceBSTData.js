de06f2641adef9b1b9b835f6323befb14ba6c72a
Antoine de Prieelle <antoine_@live.nl>
2023-08-03 14:45:46 +0200

    Update rebalanceBSTData by importing react component FieldsetMessageUpdatedSuccessValue and use it for exported rebalanceBSTMessageValue variables

--- src/data/rebalanceBSTData.js
+++ src/data/rebalanceBSTData.js
@@ -1,5 +1,7 @@
+import { FieldsetMessageUpdatedSuccessValue } from '../components/FieldsetMessageUpdatedSuccessValue/FieldsetMessageUpdatedSuccessValue.js';
+
 export const rebalanceBSTButtonText = ("Rebalance BST");
-export const rebalanceBSTMessageAlreadyBalancedBSTValue = ("The BST is already balanced");
+export const rebalanceBSTMessageAlreadyBalancedBSTValue = (<FieldsetMessageUpdatedSuccessValue icon={true} message={"The BST is already balanced"} />);
 export const rebalanceBSTMessageEmptyValue = (<>&nbsp;</>);
-export const rebalanceBSTMessageUpdatedAlreadyBalancedBSTRebalancedValue = ("The already balanced BST has been rebalanced");
-export const rebalanceBSTMessageUpdatedRebalancedValue = ("The BST has been rebalanced");
+export const rebalanceBSTMessageUpdatedAlreadyBalancedBSTRebalancedValue = (<FieldsetMessageUpdatedSuccessValue icon={true} message={"The already balanced BST has been rebalanced"} />);
+export const rebalanceBSTMessageUpdatedRebalancedValue = (<FieldsetMessageUpdatedSuccessValue icon={true} message={"The BST has been rebalanced"} />);
